c276d4be7f7f8747e297fdee4d35f970f1254a78
mwafa <wafax.4@gmail.com>
2018-03-07 06:27:42 +0700

    update js

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,11 @@
 const warna = document.querySelectorAll("input[d='satu']");
 
-
-warna.addEventListener("input",function(){
-    const r = document.querySelector("input[name='merah']d['satu']").value;
-    const g = document.querySelector("input[name='hijau']d['satu']").value;
-    const b = document.querySelector("input[name='biru']d['satu']").value;
-    console.log(r,g,b);
-    document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+warna.forEach(function(){
+    this.addEventListener("input",function(){
+        const r = document.querySelector("input[name='merah']d['satu']").value;
+        const g = document.querySelector("input[name='hijau']d['satu']").value;
+        const b = document.querySelector("input[name='biru']d['satu']").value;
+        console.log(r,g,b);
+        document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+    })
 })
